d0296d78016e8864be8020f1e234669e0d04705b
User <User@172.16.178.85>
2024-04-04 15:14:34 +0530

    Char Program

---
+++ C_37CoreJava/src/heloo/Typecasting.java
@@ -0,0 +1,29 @@
+package heloo;
+
+public class Typecasting {
+
+	public static void main(String[] args) {
+
+	char a='A';
+System.out.println(a);
+
+	char se=12;
+
+	System.out.println(se);
+
+	
+
+	char var2='\u20AC';
+
+	System.out.println(var2);
+
+	
+
+	char var1 ='\u00A7';
+	 System.out.println(var1);
+
+
+	}
+}
+
+

6730ae84c6853d893456d6b6d32d6fa37615a609
User <User@Pagukutty>
2024-04-17 09:10:12 +0530

    Pattern

---
+++ Pattern/src/org/pattern/Numbers.java
@@ -0,0 +1,16 @@
+package org.pattern;
+
+public class Numbers {
+	static int n=5;
+
+	    public static void main(String[] args) {
+	        for (int i = 1; i<=n; i++) {
+	            for (int j = 1; j <= i; j++) { 
+	                System.out.print(j);
+	            }
+	            System.out.println();
+	        }
+
+
+}
+}
